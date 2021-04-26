8e047d551158d3761fbb2a33faed5ae22b74010e
Andre Baptista <albs_br@yahoo.com.br>
2021-04-25 22:53:15 -0300

    v.0.7.1

---
+++ js/animation.js
@@ -0,0 +1,74 @@
+let timer;
+
+let player = {
+    currentFrame: 0,
+    fps: 10,
+    isPlaying: true
+};
+
+function doAnimation() {
+
+	showCurrentFrame();
+
+	nextFrame();
+
+	timer = window.setTimeout(doAnimation, 1000/player.fps);
+}
+
+function showCurrentFrame() {
+	$('#divAnim').html($('#preview' + player.currentFrame).html());
+
+	let sourceCanvas = $('canvas', '#preview' + player.currentFrame);
+	let destinationCanvas = $('canvas', '#divAnim');
+
+	var destCtx = destinationCanvas[0].getContext('2d');
+
+	destCtx.drawImage(sourceCanvas[0], 0, 0);
+}
+
+function nextFrame() {
+	player.currentFrame++;
+    
+	if(player.currentFrame == 4) { 
+        player.currentFrame = 0;
+    }
+}
+
+function prevFrame() {
+	player.currentFrame--;
+    
+	if(player.currentFrame < 0) { 
+        player.currentFrame = 3;
+    }
+}
+
+$(() => {
+    $('#btnPause').click(() => {
+        window.clearInterval(timer);
+        player.isPlaying = false;
+    });
+    
+    $('#btnPlay').click(() => {
+        if(player.isPlaying) return;
+
+        doAnimation();
+
+        player.isPlaying = true;
+
+        //$(this).disable();
+    });
+    
+    $('#btnNext').click(() => {
+        window.clearInterval(timer);
+        nextFrame();
+        showCurrentFrame();
+    });
+    
+    $('#btnPrev').click(() => {
+        window.clearInterval(timer);
+        prevFrame();
+        showCurrentFrame();
+    });
+    
+    doAnimation();
+});
